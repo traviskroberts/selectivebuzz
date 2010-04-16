4620aa7f322b44875b1a42fa88c6f33430ab0470
Travis Roberts <traviskroberts@gmail.com>
2010-04-16 08:32:41 -0500

    Fixed url in emails.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,14 +3,14 @@ class UserMailer < ActionMailer::Base
     setup_email(user)
     @subject    += 'Please activate your new account'
   
-    @body[:url]  = "http://selectivebuzz.travisroberts.net/activate/#{user.activation_code}"
+    @body[:url]  = "http://selectivebuzz.com/activate/#{user.activation_code}"
   
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://selectivebuzz.travisroberts.net/"
+    @body[:url]  = "http://selectivebuzz.com/"
   end
   
   protected
